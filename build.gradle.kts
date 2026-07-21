97bbc4ac7b2ba28106f0d4e2398fed8df09accbf
Daniil <pavlov.dv22@gmail.com>
2026-07-21 12:55:24 +0400

    feat(detekt): implemented code analyzer

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,16 @@ plugins {
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.detekt)
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    allRules = false
+    config.setFrom(files("config/detekt/detekt.yml"))
+    parallel = true
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    jvmTarget = "11"
 }
